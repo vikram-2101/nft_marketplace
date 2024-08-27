a65b8e164d8cd9724035189c4ab07eea6d020df7
Vikram Kumar <vk8969338090@gmail.com>
2024-08-27 10:42:42 +0530

    add footer section

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import { NavBar } from "../components/componentsindex";
+import { NavBar, Footer } from "../components/componentsindex";
 import "../styles/globals.css";
 const MyApp = ({ Component, pageProps }) => (
   <div>
     <NavBar />
     <Component {...pageProps} />
+    <Footer />
   </div>
 );
 
