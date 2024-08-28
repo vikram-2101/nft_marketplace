300cda5b27bcfc216fb42e6a4a47fbbfe6d8d4d1
Vikram Kumar <vk8969338090@gmail.com>
2024-08-28 13:51:35 +0530

    Add BigNFTSlider

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -3,5 +3,20 @@ import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
-
-export { Button, Footer, NavBar, HeroSection, Service };
+import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
+import Subscribe from "./Subscribe/Subscribe";
+import Title from "./Title/Title";
+import Category from "./Category/Category";
+import Filter from "./Filter/Filter";
+export {
+  Button,
+  Footer,
+  NavBar,
+  HeroSection,
+  Service,
+  BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Style from "../styles/index.module.css";
-import { HeroSection, Service } from "../components/componentsindex";
+import {
+  HeroSection,
+  Service,
+  BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+} from "../components/componentsindex";
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
+      <BigNFTSlider />
+      <Subscribe />
+      <Title />
+      <Category />
+      <Filter />
     </div>
   );
 };
